61f08bdf610874136ca4c7f1539a30934e1780b9
Ben Pfaff <blp@feldera.com>
2023-08-15 09:01:28 -0700

    work

--- crates/dbsp/src/algebra/floats.rs
+++ crates/dbsp/src/algebra/floats.rs
@@ -21,6 +21,7 @@ macro_rules! float {
             #[size_of(skip_all)]
             #[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
             #[cfg_attr(feature = "with-serde", serde(transparent))]
+            #[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
             pub struct $outer(OrderedFloat<$inner>);
 
             impl $outer {
